4f5f42266b0435e37cd64b327a25e2488601d047
Martin <2825317989@qq.com>
2020-04-28 18:44:35 +0800

    Spring MVC idea 基本demo配置

---
+++ src/main/java/com/learn/HelloWorldController.java
@@ -0,0 +1,14 @@
+package com.learn;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/helloWorld")
+public class HelloWorldController {
+    @RequestMapping("/sayHello.do")
+    public String sayHello(){
+        System.out.println("helloWorld...");
+        return "success";
+    }
+}
